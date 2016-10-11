276dd84a4295782c594be311a9844a9834eb8be8
John Miller <johnlukemiller@gmail.com>
2016-10-11 15:44:47 -0700

    Comments merge_sort_solve.py and adds file output write function

--- ass_1/merge_sort_solve.py
+++ ass_1/merge_sort_solve.py
@@ -1,15 +1,38 @@
+# CS325 - Group Assignment1
+# 2016-10-11
+# John Miller, Shane Barrantes, Griffin Gonsalves, Steven Powers
+#
+# Finds the solution to the line intersections problem in nlog(n) runtime
+
 import sys
 import csv
 
 def getFileContents(filePath):
+    """ Gets the contents of an input file and return it
+    filePath -- Path to the input file containing test data
+    """
     with open(filePath, 'rb') as f:
         reader = csv.reader(f)
         fileContents = list(reader)
     return fileContents
 
+def writeOutputFile(value):
+    f = open('output.txt', 'w')
+    f.write(str(value))
+    f.close()
+
 def mergeSort(arr, beg, end):
+    """ Recursive Merge Sort algorithm used as basis for solution to line intersection problem and
+        returns the number of line intersections
+    arr -- Array containing input data. Should be 'p' values (third line of input file)
+    beg -- Index of the 'start' of the array
+    end -- Index of the 'end' of the array
+    """
+    # Count of values moved in left half of original array
     left = 0
+    # Count of values moved in right half of original array
     right = 0
+    # Count of values moved while merging left and right halfs of original array
     count = 0
     mid = 0
 
@@ -22,12 +45,30 @@ def mergeSort(arr, beg, end):
     return left + right + count
 
 def still_merging(left, right, mid, end):
+    """ Checks to see if the algorithm should still be merging the two target arrays together and
+        returns true/false if it should/shouldn't
+    left -- The index of the current value being compared in the left array
+    right -- The index of the current value being compared in the right array
+    mid -- The 'end' of the left array
+    end -- The 'end' of the right array
+    """
     return left <= mid and right <= end
 
 def merge(arr, beg, mid, end):
+    """ Merges two given array and returns the number of positions a value in the
+        'right' array has to move when merging into the 'left' array
+    arr -- Array containing input data. Should be 'p' values (third line of input file)
+    beg -- Index of the 'start' of the array
+    mid -- The 'end' of the left array
+    end -- The 'end' of the right array
+    """
+    # Temporary array used to store merged values
     tempArr = [0] * len(arr)
+    # Current index being examined in the 'left' array
     left = beg
+    # Current index being examined in the 'right' array
     right = mid + 1
+    # Index used to track where sorted values should be placed in tempArr
     cur = left
     intersections = 0
 
@@ -57,4 +98,4 @@ def merge(arr, beg, mid, end):
     return intersections
 
 fileContents = map(int, getFileContents(sys.argv[1])[2])
-print mergeSort(fileContents, 0, len(fileContents) - 1)
+writeOutputFile(mergeSort(fileContents, 0, len(fileContents) - 1))
