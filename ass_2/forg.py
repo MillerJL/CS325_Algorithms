5dccd3106804bcd2b60249d0b673584b0ca046d4
John Miller <johnlukemiller@gmail.com>
2016-10-24 19:20:48 -0700

    Fixes code style

--- ass_2/forg.py
+++ ass_2/forg.py
@@ -4,6 +4,7 @@ import sys
 
 sys.setrecursionlimit(10000000)
 
+
 def getFileContents(filePath):
     result = []
     with open(filePath, 'r') as f:
@@ -18,26 +19,31 @@ def getFileContents(filePath):
     result[5] = map(int, result[5].split(','))
     return result
 
+
 def Jump(P, Q, leash):
-  P_len = len(P) - 1
-  Q_len = len(Q) - 1
+    P_len = len(P) - 1
+    Q_len = len(Q) - 1
 
-  if P_len == 0 and Q_len == 0:
-    return True
+    if distance(P[P_len], Q[Q_len]) > leash:
+        return False
 
-  if (P_len > 0 and Q_len > 0) and (distance(P[P_len - 1], Q[Q_len - 1]) <= leash):
-    return Jump(P[0:P_len], Q[0:Q_len], leash)
+    if P_len == 0 and Q_len == 0:
+        return True
 
-  if P_len > 0 and (distance(P[P_len - 1], Q[Q_len]) <= leash):
-    return Jump(P[0:P_len], Q, leash)
+    if (P_len > 0 and Q_len > 0) and (distance(P[P_len - 1], Q[Q_len - 1]) <= leash):
+        return Jump(P[0:P_len], Q[0:Q_len], leash)
 
-  if Q_len > 0 and (distance(P[P_len], Q[Q_len - 1]) <= leash):
-    return Jump(P, Q[0:Q_len], leash)
+    if P_len > 0 and (distance(P[P_len - 1], Q[Q_len]) <= leash):
+        return Jump(P[0:P_len], Q, leash)
 
-  return False
+    if Q_len > 0 and (distance(P[P_len], Q[Q_len - 1]) <= leash):
+        return Jump(P, Q[0:Q_len], leash)
+
+    return False
+
 
 def distance(P, Q):
     return int(math.ceil(math.hypot((abs(P[0] - Q[0])), (abs(P[1] - Q[1])))))
 
 fileContents = getFileContents(sys.argv[1])
-print Jump(fileContents[1], fileContents[3], 227)
+print Jump(fileContents[1], fileContents[3], 10)
